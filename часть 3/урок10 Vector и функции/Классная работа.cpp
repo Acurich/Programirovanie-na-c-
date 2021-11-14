11beaf492c9e349859066c38cd27305924989cad
Acurich <91753821+Acurich@users.noreply.github.com>
2021-11-14 19:32:52 +0300

    Update Классная работа.cpp

--- часть 3/урок10 Vector и функции/Классная работа.cpp
+++ часть 3/урок10 Vector и функции/Классная работа.cpp
@@ -18,3 +18,31 @@ int main() {
   
 }
 }
+
+//Напишите программу, которая для двух векторов поменяет значения между ними местами.
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+
+int main() {
+int n1, n2;
+  cin>> n1;
+  vector <int> a(n1);
+  for (int i = 0; i <n1; i++){
+    cin >> a[i];
+  }
+  cin >> n2;
+  vector <int> b(n2);
+  for (int i = 0; i < n2; i++) {
+    cin >> b[i] ;
+  }
+  swap(a,b);
+    
+  for (int i = 0; i < a.size(); i++)
+    cout << a[i] << " ";
+  cout << endl;
+  for (int i = 0; i < b.size(); i++)
+    cout << b[i] << " ";
+  return 0;
+}
