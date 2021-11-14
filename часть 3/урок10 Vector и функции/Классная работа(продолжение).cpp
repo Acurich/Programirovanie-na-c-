8bea53dc22688d9f10a84566617b53475c464b49
Acurich <91753821+Acurich@users.noreply.github.com>
2021-11-14 19:41:33 +0300

    Update Классная работа(продолжение).cpp

--- часть 3/урок10 Vector и функции/Классная работа(продолжение).cpp
+++ часть 3/урок10 Vector и функции/Классная работа(продолжение).cpp
@@ -17,3 +17,25 @@ int main() {
   print(a);
   return 0;
 }
+
+//Создайте функцию vector<int> read_vector(), считывающую последовательность чисел и возвращающую эту последовательность как результат работы функции.
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+vector<int> read_vector() {
+    vector<int> v;
+    int n, tmp;
+    cin >> n;
+    while (n--) {
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    return v;
+}
+int main() {
+  vector<int> a = read_vector();
+  for (int i = 0; i < a.size(); i++)
+    cout << a[i] << " ";
+  return 0;
+}
