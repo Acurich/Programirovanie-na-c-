6de002f1c618212e1cf126101bf4f82ede9e7d1a
Acurich <91753821+Acurich@users.noreply.github.com>
2021-11-20 16:21:47 +0300

    Update Домашняя работа.cpp

--- часть 3/урок10 Vector и функции/Домашняя работа.cpp
+++ часть 3/урок10 Vector и функции/Домашняя работа.cpp
@@ -1,1 +1,23 @@
+//Напишите функцию void fill(vector<int> &a, int b = 0), которая заполняет вектор заданными значениями или нулем, если параметр не указан.
+#include <iostream>
+#include <vector>
+using namespace std;
+void fill(vector<int>& box, int x = 0) {
+for (auto& v : box) v = x;
+}
+int main(){
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    fill(a, k);
+    for (int i = 0; i < n; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    fill(a);
+    for (int i = 0; i < n; i++){
+        cout << a[i] << " ";
+    }
+    return 0;
+}
 
