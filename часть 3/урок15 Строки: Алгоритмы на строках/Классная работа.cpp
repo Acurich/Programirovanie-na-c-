9358eb46c8c6d5e19dd6befae655214359fe9a80
Acurich <91753821+Acurich@users.noreply.github.com>
2021-12-26 23:00:22 +0300

    Update Классная работа.cpp

--- часть 3/урок15 Строки: Алгоритмы на строках/Классная работа.cpp
+++ часть 3/урок15 Строки: Алгоритмы на строках/Классная работа.cpp
@@ -1,1 +1,34 @@
+//Дана строка S, состоящая из строчных латинских букв. Ваша задача — определить, совпадают ли первый и последний символы этой строки.
+#include <iostream>
+using namespace std;
 
+int main() {
+  string s;
+  cin >> s;
+  if (s[s.size() - 1] == s[0])
+    cout << "YES";
+  else
+    cout << "NO";
+  return 0;
+}
+
+//Вам дана строка S, содержащая только строчные латинские буквы. Ваша задача — заменить в ней все буквы «e» на «i».
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+int main() {
+ string s;
+ int k = 0;
+ getline(cin, s);
+ for (int i = 0; i < s.size(); i++) {
+  if (s[i] == 'e') {
+   s[i] = 'i';
+  }
+ }
+ cout << s;
+ system("pause");
+ return 0;
+}
+
+//
